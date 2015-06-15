cd9df336856c83ed68a76b964a5b94cb9ff6ecd0
Sandro Mathys <sandro@midokura.com>
2015-06-15 17:54:39 +0900

    Have update.sh deal with SELinux as git won't
    Change-Id: I46140e9c798a0ef4c31e6e0c513bc94674741400

--- update.sh
+++ update.sh
@@ -3,5 +3,12 @@ cd /root/planet-midonet/
 # We don't pull since we don't want to merge. Overwrite!
 git fetch
 git reset --hard origin/master
+# These configuration files are symlinked from the proper locations,
+# but git isn't SELinux aware
+chcon -t system_cron_spool_t cronjob
+chcon -t httpd_config_t      httpd-planet.conf
+# Just in case the configuration changed...
+# (note: crond reloads changed files automatically)
+systemctl reload httpd
 # Update the planet
 /bin/python /root/planet-midonet/planet.py /root/planet-midonet/midonet.ini
