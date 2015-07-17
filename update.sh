b3a30e90b7a8e0d4edff53b05872caae7068c277
Sandro Mathys <sandro@midokura.com>
2015-07-17 15:02:54 +0900

    Improve config/SELinux handling
    Change-Id: I2dfdbcab4deb40d23deb4d99c8c1d9f2ed3c29bf

--- update.sh
+++ update.sh
@@ -3,12 +3,13 @@ cd /root/planet-midonet/
 # We don't pull since we don't want to merge. Overwrite!
 git fetch
 git reset --hard origin/master
-# These configuration files are symlinked from the proper locations,
-# but git isn't SELinux aware
-chcon -t system_cron_spool_t cronjob
-chcon -t httpd_config_t      httpd-planet.conf
-# Just in case the configuration changed...
-# (note: crond reloads changed files automatically)
+# update cronjob in case it changed
+# note: crond reloads changed config automatically
+# (use cp instead of ln/mv to avoid SELinux issues)
+cp -f cronjob           /etc/cron.d/planet
+# update httpd config in case it changed
+# (use cp instead of ln/mv to avoid SELinux issues)
+cp -f httpd-planet.conf /etc/httpd/conf.d/planet.conf
 systemctl reload httpd
 # Update the planet
 /bin/python /root/planet-midonet/planet.py /root/planet-midonet/midonet.ini
